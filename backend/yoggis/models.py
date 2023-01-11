85b2599928c574fde8d2428324ce261e973880dd
Krishbin Paudel <krishbin21@gmail.com>
2023-01-11 22:07:45 +0545

    yoggis.models: updated models to fit the user needs

--- backend/yoggis/models.py
+++ backend/yoggis/models.py
@@ -1,12 +1,39 @@
 from django.db import models
 
-# Create your models here.
+
+class UserDisorder(models.Model):
+    type = models.CharField(max_length=40)
+    description = models.TextField()
+    user_disorder_image = models.ImageField(upload_to='userDisorderImages/')
+
+    def __str__(self):
+        return self.type
+
+
+class YogaCategory(models.Model):
+    type = models.CharField(max_length=40)
+    description = models.TextField()
+    category_image = models.ImageField(upload_to='yogaCategoryImages/')
+
+    def __str__(self):
+        return self.type
+
 
 class Yoga(models.Model):
-    
     title = models.CharField(max_length=50)
-    description = models.CharField(max_length=200)
-    image = models.ImageField(upload_to='yoga_images/')
+    description = models.CharField(max_length=250)
+    image = models.ImageField(upload_to='yogaImages/')
+    # age group
+    lower_age = models.IntegerField(default=0)
+    upper_age = models.IntegerField(default=0)
+    yoga_category = models.ManyToManyField(YogaCategory)
+    avoid_for_disorder = models.ManyToManyField(UserDisorder)
 
     def __str__(self):
         return self.title
+
+
+class CorrectVectorLocations(models.Model):
+    angle_location = models.CharField()
+    angle_value = models.FloatField()
+    angle_of = models.ForeignKey(Yoga, on_delete=models.CASCADE)
