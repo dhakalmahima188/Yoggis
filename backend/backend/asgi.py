f9bec08c3caf7aabeacb9ff9e0b188b0965579c8
Krishbin Paudel <krishbin21@gmail.com>
2023-01-13 16:48:23 +0545

    websocket: implemented websocket to send data on correction of yoga
    now we can directly broadcast data to the server with test channel layer

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -10,7 +10,21 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from channels.security.websocket import AllowedHostsOriginValidator
+import yoggis.routing as rt
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter(
+    {
+        'http': get_asgi_application(),
+        'websocket': AllowedHostsOriginValidator(AuthMiddlewareStack(
+            URLRouter(
+                rt.websocket_urlpatterns
+            )
+        )
+        ),
+    }
+)
